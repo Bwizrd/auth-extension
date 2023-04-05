0095d53888688b165b0ad84d7358d14ce931309c
Paul <paul@pans-house.com>
2023-04-05 11:54:51 +0100

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CoffeesModule } from './coffees/coffees.module';
+import { UsersModule } from './users/users.module';
+import { IamModule } from './iam/iam.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'pass123',
+    database: 'postgres',
+    autoLoadEntities: true,
+    synchronize: true,
+  }),
+    CoffeesModule, UsersModule, IamModule,
+    ConfigModule.forRoot()
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
